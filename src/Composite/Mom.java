a1be7bcadf2ffa4919738e0c7b7cdf706fee8006
Bush <adyurbash@mail.ru>
2023-01-03 18:22:48 +0300

    Composite

---
+++ src/Composite/Mom.java
@@ -0,0 +1,8 @@
+package Composite;
+
+public class Mom implements Girls{
+    @Override
+    public void kitchenDeals() {
+        System.out.println("Mom wash up");
+    }
+}
---
+++ src/Composite/Sister.java
@@ -0,0 +1,8 @@
+package Composite;
+
+public class Sister implements Girls{
+    @Override
+    public void kitchenDeals() {
+        System.out.println("Sister wash up");
+    }
+}
